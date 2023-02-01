d84f95f5d05725055b077879063617577a2d9a10
Jayabrata <joyj03787@gmail.com>
2023-02-01 17:34:19 +0530

    input and variabbles usages

---
+++ input.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#User Input
+read -p "Enter your name: " NAME
+echo "Your name is $NAME"
+
+echo "Please enter your full name: "
+read FNAME LNAME
+echo "Yor name is $FNAME $LNAME"
+
+echo "Please enter yor name and age: "
+read FNAME AGE 
+echo "Yor name is $FNAME and you are $AGE years old."
---
+++ variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+NAME="Jayabrata"
+SPORT="Foot"
+echo "My name is $NAME"
+echo "The most popular sport is ${SPORT}ball"
+
+STUDENT_01="Biru"
